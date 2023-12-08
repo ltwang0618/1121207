24d11a480a91f4b7acc8e2fbcd68447f9d11dc35
den <ww581564@gmail.com>
2023-12-09 00:49:23 +0800

    修改註冊js

--- js/register1-1.js
+++ js/register1-1.js
@@ -1,85 +1,88 @@
-let data = [];
+let dataList = [];
 
-
-function init(){
-    check()
-    validateEmail()
+function init() {
+    check();
 }
-init()
-//偵測帳號是否輸入文字
-function check(){
-    let emailCheck = document.querySelector('.emailCheck')
-    const btn =document.querySelector('.btn');
-btn.addEventListener('click',function(e){
-    let dateValue = date.value;
-    let passwordCheck =document.querySelector('.passwordCheck').value;
-    let password = document.querySelector('.password').value;
-    let email = document.querySelector('.email').value;
-    let username = document.querySelector('.userName').value;
-    if (!validateEmail(email)){
-        emailCheck.innerHTML='請輸入有效電子郵件'
-        return;
-    }if(email != 0&& password != 0 && password===passwordCheck&& dateValue !=0){
-    data={
-        account:email,
-        password:password,
-        userName:username,
-        birthday:dateValue
-    };
-    alert('註冊成功')
-    emailCheck.innerHTML='';
-    let jsonData= JSON.stringify(data);
-    console.log(jsonData)
-    return;
-   }else{
-    alert('帳戶或密碼輸入錯誤')
-    }
-})
-//偵測使用者名稱是否輸入文字
-let messageuserName=document.querySelector('.messageuserName')
-btn.addEventListener('click',function(e){
-    let username = document.querySelector('.userName').value;
-    let str = ''
-    if(username==0){
-        str+='請輸入要註冊的名稱'
-    }else{
-    messageuserName.innerHTML=str
-    data.userName=username;}
 
-})
-//偵測密碼是否輸入文字
-let messagepassword=document.querySelector('.messagepassword')
-btn.addEventListener('click',function(e){
-    let password = document.querySelector('.password').value;
-    let str = ''
-    if(password==0){
-        str+='請輸入要註冊的密碼'
-    }
-    messagepassword.innerHTML=str
-})
-//偵測重複密碼是否一樣
-let messagepasswordCheck=document.querySelector('.messagepasswordCheck')
-btn.addEventListener('click',function(e){
-    let password = document.querySelector('.password').value;
-    let passwordCheck = document.querySelector('.passwordCheck').value;
-    let str = ''
-    if(passwordCheck!==password){
-        str+='請輸入相同密碼'
-    }
-    messagepasswordCheck.innerHTML=str
-})
+init();
 
-const date = document.querySelector('.date');
-btn.addEventListener('click',function(e){
-let str = '';
-let dateValue = date.value;
-str=dateValue
-data.birthday = str;
-})
-     
+function check() {
+    let emailCheck = document.querySelector('.emailCheck');
+    const btn = document.querySelector('.btn');
+
+    btn.addEventListener('click', function (e) {
+        let dateValue = date.value;
+        let passwordCheck = document.querySelector('.passwordCheck').value;
+        let password = document.querySelector('.password').value;
+        let email = document.querySelector('.email').value;
+        let username = document.querySelector('.userName').value;
+
+        if (!validateEmail(email)) {
+            emailCheck.innerHTML = '請輸入有效電子郵件';
+            return;
+        }
+
+        if (email != 0 && password != 0 && password === passwordCheck && dateValue != 0) {
+            let userData = {
+                account: email,
+                password: password,
+                userName: username,
+                birthday: dateValue
+            };
+            dataList.push(userData);
+            alert('註冊成功');
+            emailCheck.innerHTML = '';
+            localStorage.setItem('userDataList', JSON.stringify(dataList));
+        } else {
+            alert('帳戶或密碼輸入錯誤');
+        }
+    });
+
+    let messageuserName = document.querySelector('.messageuserName');
+    btn.addEventListener('click', function (e) {
+        let username = document.querySelector('.userName').value;
+        let str = '';
+
+        if (username == 0) {
+            str += '請輸入要註冊的名稱';
+        } else {
+            messageuserName.innerHTML = str;
+        }
+    });
+
+    let messagepassword = document.querySelector('.messagepassword');
+    btn.addEventListener('click', function (e) {
+        let password = document.querySelector('.password').value;
+        let str = '';
+
+        if (password == 0) {
+            str += '請輸入要註冊的密碼';
+        }
+        messagepassword.innerHTML = str;
+    });
+
+    let messagepasswordCheck = document.querySelector('.messagepasswordCheck');
+    btn.addEventListener('click', function (e) {
+        let password = document.querySelector('.password').value;
+        let passwordCheck = document.querySelector('.passwordCheck').value;
+        let str = '';
+
+        if (passwordCheck !== password) {
+            str += '請輸入相同密碼';
+        }
+        messagepasswordCheck.innerHTML = str;
+    });
+
+    const date = document.querySelector('.date');
+    btn.addEventListener('click',function(e){
+        let str = '';
+        let dateValue = date.value;
+        str = dateValue;
+        dataList[dataList.length - 1].birthday = str;
+    });
 }
+
 function validateEmail(email) {
-    // 使用簡單的正則表達式檢查電子郵件格式
     let emailRegex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return emailRegex.test(email);
 }
